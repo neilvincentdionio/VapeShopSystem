71e5e28f7cddea71d3bba2b1d26f0bec9dbe1d8b
jed0125 <jedvalenzuela269@gmail.com>
2026-04-09 22:44:37 +0800

    Revert "Add security features: JWT auth, encryption, backup system, audit logging"
    This reverts commit d6938426a95b033f3a06e9cf5bdfc85b35d5bb8e.

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -35,7 +35,6 @@ class Filters extends BaseFilters
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
         'auth'          => \App\Filters\AuthFilter::class,
-        'jwtauth'       => \App\Filters\JwtAuthFilter::class,
     ];
 
     /**
